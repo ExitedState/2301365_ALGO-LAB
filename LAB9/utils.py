1cc6953f0e3b3b8d4b4355117f377ded8c066a98
Phaiboon A <phaiboon760@gmail.com>
2023-11-10 10:19:34 +0700

    solution_1

--- LAB9/utils.py
+++ LAB9/utils.py
@@ -2,10 +2,6 @@ def read_test_cases_from_txt(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
-    # Check if file has the expected number of lines
-    if len(lines) < 4:
-        raise ValueError("The file does not contain enough lines for a test case.")
-
     # Parse the alphabet set
     alphabet_set = lines[0].strip().split()
 
@@ -16,14 +12,9 @@ def read_test_cases_from_txt(filename):
 
     # Parse the pattern
     pattern = ''.join(lines[2].split())
-    if len(pattern) != pattern_length:
-        raise ValueError("The pattern length does not match the specified length.")
 
     # Parse the text
     text = ''.join(lines[3].split())
-    if len(text) != text_length:
-        raise ValueError("The text length does not match the specified length.")
-
     return alphabet_set, pattern, text
 
 if __name__ == '__main__':
